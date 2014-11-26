83252838c8c9a89865207e1db0ea1dc61637c663
kanakogi <kanakogi@gmail.com>
2014-11-26 13:55:11 +0900

    管理画面のHTMLを調整

--- admin/admin.php
+++ admin/admin.php
@@ -23,11 +23,11 @@ unset(
 	$post_taxonomies['post_format']
 );
 ?>
-
+<div class="wrap">
 <script type="text/javascript">
 jQuery(function($){
 
-	//カレンダー
+	//カレンダー / 公開日
     var post_dates = jQuery( '#post_date-datepicker-from, #post_date-datepicker-to' ) . datepicker( {
         showAnim: 'clip',
         changeMonth: true,
@@ -45,6 +45,7 @@ jQuery(function($){
     } );
     post_dates.datepicker("option", "dateFormat", 'yy-m-d');
 
+    //カレンダー / 更新日
     var post_modifieds = jQuery( '#post_modified-datepicker-from, #post_modified-datepicker-to' ) . datepicker( {
         showAnim: 'clip',
         changeMonth: true,
@@ -61,6 +62,16 @@ jQuery(function($){
         }
     } );
     post_modifieds.datepicker("option", "dateFormat", 'yy-m-d');
+
+    //チェックボックス
+    $('.all_checked').click(function(){ 
+        var target = $(this).attr('data-target');
+        $(target).prop('checked', true);
+    });
+    $('.all_checkout').click(function(){ 
+        var target = $(this).attr('data-target');
+        $(target).prop('checked', false);
+    });
 
 <?php foreach ( $post_types as $post_type ):?>
 $('#form_<?php echo esc_attr( $post_type->name ) ?>').submit(function(){
@@ -92,31 +103,43 @@ $('#form_<?php echo esc_attr( $post_type->name ) ?>').submit(function(){
 <?php foreach ( $post_types as $post_type ):?><div class="plugin_content">
 <form action="<?php echo WCE_PLUGIN_URL .'/admin/' ?>download.php" method="post" id="form_<?php echo esc_attr( $post_type->name ) ?>" target="_blank">
 <?php echo wp_nonce_field( 'csv_export' );?>
-<ul>
+<div class="tool-box">
+<h3>設定</h3>
+<ul class="setting_list">
 <li><label><input type="radio" name="post_id" value="post_id" checked="checked" required>*投稿ID</label></li>
 <li><label><input type="radio" name="post_type" value="<?php echo esc_attr( $post_type->name ) ?>" checked="checked" required>*投稿タイプ</label></li>
-<li><label><input type="checkbox" name="posts_value[]" value="post_name">スラッグ</label></li>
-<li><label><input type="checkbox" name="posts_value[]" value="post_title">タイトル</label></li>
-<li><label><input type="checkbox" name="posts_value[]" value="post_content">本文</label></li>
+<li><label><input type="checkbox" name="posts_value[]" value="post_name" checked="checked">スラッグ</label></li>
+<li><label><input type="checkbox" name="posts_value[]" value="post_title" checked="checked">タイトル</label></li>
+<li><label><input type="checkbox" name="posts_value[]" value="post_content" checked="checked">本文</label></li>
 <li>ステータス
 <ul>
-<label><input type="checkbox" name="post_status[]" value="publish" class="post_status" checked="checked">公開済み</label></li>
-<label><input type="checkbox" name="post_status[]" value="pending" class="post_status" >レビュー待ち</label></li>
-<label><input type="checkbox" name="post_status[]" value="draft" class="post_status" >下書き</label></li>
-<label><input type="checkbox" name="post_status[]" value="future" class="post_status" >スケジュール済み</label></li>
-<label><input type="checkbox" name="post_status[]" value="private" class="post_status" >非公開</label></li>
-<label><input type="checkbox" name="post_status[]" value="trash" class="post_status" >ゴミ箱入り</label></li>
-<label><input type="checkbox" name="post_status[]" value="inherit" class="post_status" >inherit</label></li>
+<li><label><input type="checkbox" name="post_status[]" value="publish" class="post_status" checked="checked">公開済み</label></li>
+<li><label><input type="checkbox" name="post_status[]" value="pending" class="post_status" >レビュー待ち</label></li>
+<li><label><input type="checkbox" name="post_status[]" value="draft" class="post_status" >下書き</label></li>
+<li><label><input type="checkbox" name="post_status[]" value="future" class="post_status" >スケジュール済み</label></li>
+<li><label><input type="checkbox" name="post_status[]" value="private" class="post_status" >非公開</label></li>
+<li><label><input type="checkbox" name="post_status[]" value="trash" class="post_status" >ゴミ箱入り</label></li>
+<li><label><input type="checkbox" name="post_status[]" value="inherit" class="post_status" >inherit</label></li>
 </ul>
 <li><label><input type="checkbox" name="posts_value[]" value="post_author">投稿者</label></li>
 <li><label><input type="checkbox" name="posts_value[]" value="post_date">公開日時</label></li>
+<li><label><input type="checkbox" name="posts_value[]" value="post_modified">更新日時</label></li>
 <li><label><input type="checkbox" name="post_tags" value="post_tags">タグ</label></li>
 </ul>
+</div>
 
 <hr>
 
-<h3><?php echo $post_type->labels->name ?>のタクソノミー</h3>
-<ul>
+<div class="tool-box">
+<h3><?php echo $post_type->labels->name ?>のタクソノミー
+<span class="all_checks">
+[
+<a href="javascript:void(0);" class="all_checked" data-target="#form_<?php echo esc_attr( $post_type->name ) ?> .cf_taxonomy">全選択</a>
+<a href="javascript:void(0);" class="all_checkout" data-target="#form_<?php echo esc_attr( $post_type->name ) ?> .cf_taxonomy">全解除</a>
+]
+</span>
+</h3>
+<ul class="setting_list">
 <?php
 $num = 0;
 foreach ( $post_taxonomies as $post_taxonomy ):
@@ -126,7 +149,7 @@ foreach ( $post_taxonomies as $post_taxonomy ):
 		continue;
 	}
 ?>
-	<li><label><input type="checkbox" name="taxonomies[]" value="<?php echo $post_taxonomy->name;?>" <?php selected( $_GET['engine'], '1000' );?>> <?php echo $post_taxonomy->labels->name;?></label></li>
+	<li><label><input type="checkbox" name="taxonomies[]" class="cf_taxonomy" checked="checked" value="<?php echo $post_taxonomy->name;?>" <?php selected( $_GET['engine'], '1000' );?>> <?php echo $post_taxonomy->labels->name;?></label></li>
 <?php
 $num ++;
 endforeach;
@@ -136,53 +159,80 @@ if ( $num == 0 ):
 <li>登録されているカスタムタクソノミーはありません。</li>
 <?php endif; ?>
 </ul>
+</div>
 
 <hr>
 
-<h3><?php echo $post_type->labels->name ?>のカスタムフィールド</h3>
+<div class="tool-box">
+<h3><?php echo $post_type->labels->name ?>のカスタムフィールド
+<span class="all_checks">
+[
+<a href="javascript:void(0);" class="all_checked" data-target="#form_<?php echo esc_attr( $post_type->name ) ?> .cf_checkbox">全選択</a>
+<a href="javascript:void(0);" class="all_checkout" data-target="#form_<?php echo esc_attr( $post_type->name ) ?> .cf_checkbox">全解除</a>
+]
+</span>
+</h3>
+
 <?php
 //カスタムフィールドリストを取得
 $cf_results = $this->get_custom_field_list( $post_type->name );
 ?>
-<ul>
+<ul class="setting_list">
 <?php if ( !empty( $cf_results ) ): ?>
 <?php foreach ( $cf_results as $key => $value ) :?>
-<li><label><input type="checkbox" name="cf_fields[]" value="<?php echo $value['meta_key']; ?>"> <?php echo $value['meta_key']; ?></label></li>
+<li><label><input type="checkbox" name="cf_fields[]" class="cf_checkbox" checked="checked" value="<?php echo $value['meta_key']; ?>"> <?php echo $value['meta_key']; ?></label></li>
 <?php endforeach;?>
 <?php else: ?>
 <li>登録されているカスタムフィールドはありません。</li>
 </ul>
 <?php endif; ?>
+</div>
 
 <hr>
 
+<div class="tool-box">
 <h3>その他</h3>
-<ul>
-<li>記事数 <input type="text" name="limit" class="limit" value="0">※0の場合はすべてダウンロード</li>
-<li id="post_date-datepicker-wrap">
-期間指定: 公開日
+<table class="setting_table">
+<tbody>
+<tr>
+<th>記事数</th>
+<td><input type="text" name="limit" class="limit regular-text code" value="0">※0の場合はすべてダウンロード</td>
+</tr>
+<tr>
+    <th>"公開日"期間指定</th>
+    <td id="post_date-datepicker-wrap">
     <input type="text" id="post_date-datepicker-from" name="post_date_from"/>
     <label for="post_date-datepicker-from">から</label>
     <input type="text" id="post_date-datepicker-to" name="post_date_to"/>
     <label for="post_date-datepicker-to">まで</label>
-</li>
-
-<li id="post_modified-datepicker-wrap">
-期間指定: 更新日
+    </td>
+</tr>
+<tr>
+    <th>"更新日"期間指定</th>
+    <td id="post_modified-datepicker-wrap">
     <input type="text" id="post_modified-datepicker-from" name="post_modified_from"/>
     <label for="post_modified-datepicker-from">から</label>
     <input type="text" id="post_modified-datepicker-to" name="post_modified_to"/>
     <label for="post_modified-datepicker-to">まで</label>
-</li>
-<li>
-文字コード
-<label><input type="radio" name="string_code" value="UTF-8" checked="checked"> UTF-8</label>
-<label><input type="radio" name="string_code" value="SJIS"> Shift_JIS</label>
-</li>
+    </td>
+</tr>
+<tr>
+    <th><span>文字コード</span></th>
+    <td>
+<ul class="setting_list">
+<li><label><input type="radio" name="string_code" value="UTF-8" checked="checked"> UTF-8</label></li>
+<li><label><input type="radio" name="string_code" value="SJIS"> Shift_JIS</label></li>
 </ul>
+    </td>
+</tr>
+</tbody>
+</table>
+
+</div>
 
 <p class="submit"><input type="submit" id="post_csv" class="button-primary" value="エクスポート" /></p>
 </form>
 </div>
 <?php endforeach;?>
+</div>
 </div>
